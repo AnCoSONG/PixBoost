e44216fee0966f49adbcf086a4548dc9f84991f3
MengZhongYuan <33193572+MengYa123@users.noreply.github.com>
2018-10-23 17:43:51 +0800

    commit the static find page
    the icon is of some matter

---
+++ find.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#find_1").mouseover(function(){
+        $('#find_bg').animate({opacity:0.5},"1000");
+    });
+    $("#find_1").mouseleave(function(){
+        $('#find_bg').animate({opacity:1},"500");
+    });
+    $("#find_2").mouseover(function(){
+        $('#find_bg').animate({opacity:0.5},"1000");
+    });
+    $("#find_2").mouseleave(function(){
+        $('#find_bg').animate({opacity:1},"500");
+    });
+});
