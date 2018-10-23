e80d77f7186d5ac79f51db5c92213abdaaa85cc1
Vaskka <1139851358@qq.com>
2018-10-24 01:09:35 +0800

    添加文档和修改文件名

--- dev/js/fluid_banner.js
+++ dev/js/fluid_banner.js
@@ -1,16 +1,18 @@
 /* Vaskka 2018-10-23 15:56:50 */
 
 
-
+/**
+ * 展示页
+ * @param {String} page 
+ */
 function turn_to_show(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '7',
         top: '0px',
-        // transformScale: '1',
         opacity: '1'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '7',
             "top": '0px',
@@ -23,60 +25,72 @@ function turn_to_show(page) {
 
 }
 
+
+/**
+ * 展示下一层
+ * @param {String} page 
+ */
 function turn_to_up_1(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '6',
         top: '-60px',
-        // transformScale: '0.85',
-        opacity: '0.85'
+        opacity: '0.61'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '6',
             "top": '-60px',
             "transform": 'scale(0.85)',
-            "opacity": '0.85'
+            "opacity": '0.61'
         }
         bl.css(c_css);
     });
 
 }
 
+
+/**
+ * 展示下二层
+ * @param {String} page 
+ */
 function turn_to_up_2(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '5',
         top: '-120px',
-        // transformScale: '0.7',
-        opacity: '0.7'
+        opacity: '0.52'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '5',
             "top": '-120px',
             "transform": 'scale(0.7)',
-            "opacity": '0.7'
+            "opacity": '0.52'
         };
         bl.css(c_css);
     });
 
 }
 
+
+/**
+ * 展示下三层
+ * @param {String} page 
+ */
 function turn_to_up_3(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '4',
         top: '-180px',
-        // transformScale: '0.55',
-        opacity: '0.55'
+        opacity: '0.42'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '4',
             "top": '-180px',
             "transform": 'scale(0.55)',
-            "opacity": '0.55'
+            "opacity": '0.42'
         };
         bl.css(c_css);
     });
@@ -84,66 +98,83 @@ function turn_to_up_3(page) {
 }
 
 
+/**
+ * 展示前三层
+ * @param {String} page 
+ */
 function turn_to_down_3(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '1',
         top: '180px',
-        // transformScale: '0.55',
-        opacity: '0.55'
+        opacity: '0.42'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '1',
             "top": '180px',
             "transform": 'scale(0.55)',
-            "opacity": '0.55'
+            "opacity": '0.42'
         };
         bl.css(c_css);
     });
 
 }
 
+
+/**
+ * 展示前二层
+ * @param {String}} page 
+ */
 function turn_to_down_2(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '2',
         top: '120px',
-        // transformScale: '0.7',
-        opacity: '0.7'
+        opacity: '0.52'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '2',
             "top": '120px',
             "transform": 'scale(0.7)',
-            "opacity": '0.7'
+            "opacity": '0.52'
         };
         bl.css(c_css);
     });
 
 }
 
+
+/**
+ * 展示前一层
+ * @param {String} page 
+ */
 function turn_to_down_1(page) {
     var bl = $("[page='" + page + "']");
     var ch_css = {
         zIndex: '3',
         top: '60px',
-        // transformScale: '0.85',
-        opacity: '0.85'
+        opacity: '0.61'
     };
-    bl.animate(ch_css, 700, "swing", function() {
+    bl.animate(ch_css, 1000, "swing", function() {
         var c_css = {
             "z-index": '3',
             "top": '60px',
             "transform": 'scale(0.85)',
-            "opacity": '0.85'
+            "opacity": '0.61'
         };
         bl.css(c_css);     
     });
 
 }
 
+
+/**
+ * 根据z-index的值判断应该变换到什么层
+ * @param {String} z_index 
+ * @param {int} current_page 
+ */
 function turn_to_status(z_index, current_page) {
     switch (z_index) {
         case "1":
@@ -177,6 +208,7 @@ function turn_to_status(z_index, current_page) {
 function changeStatus() {
     var status = $("[page='0']").css("z-index")
     for (var i = 0; i < 7; i++) {
+        // 自己赋值下一个page的状态
         if (i != 6) {
             var next_status = $("[page='" + (i + 1).toString() + "']").css("z-index")
             turn_to_status(next_status, i);
@@ -194,7 +226,7 @@ function changeStatus() {
 function main() {
     setInterval(function () {
         changeStatus();
-    }, 2000);
+    }, 2500);
 }
 
 
